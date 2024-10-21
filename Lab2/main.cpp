01b365c628adf2c06fd8296aa8d61d79dde3368a
NikitaZGG <Nikitabazenov35@gmail.com>
2024-10-21 12:46:40 +0200

    Add files via upload

---
+++ Lab2/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    int p;
+    cin >> A >> B >> C;
+
+    if (A + B > C && A + C > B && B + C > A) {
+        p = (A + B + C) / 2.0;
+        cout << endl << sqrt(p * (p - A) * (p - B) * (p - C)) << endl;
+    }
+    else {
+        cout << "Треугольник не существует" << endl;
+    }
+
+    return 0;
+}
