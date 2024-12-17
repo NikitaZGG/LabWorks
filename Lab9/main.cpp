45468701019c758fecfc04342f4c6150db167b08
NikitaZGG <Nikitabazenov35@gmail.com>
2024-12-18 01:46:50 +0200

    Add files via upload

---
+++ Lab9/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+void printBytes(int number) {
+    unsigned char* bytePtr = reinterpret_cast<unsigned char*>(&number);
+
+    cout << "Содержимое байтов числа " << number << ":\n";
+
+    for (size_t i = 0; i < sizeof(int); ++i) {
+        cout << "Байт " << i << ": " << static_cast<int>(bytePtr[i]) << "\n";
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int number;
+    cout << "Введите целое число: ";
+    cin >> number;
+
+    printBytes(number); 
+    return 0;
+}
